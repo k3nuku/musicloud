1ee76db58dabe6cdfbd0b08200c7b5337d9e5f48
blupine <skwint11@gmail.com>
2018-06-11 21:06:35 +0900

    models added

--- mc/models.py
+++ mc/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import User
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+class Album(models.Model):
+    artist = models.ForeignKey(models.Artist)
+    user = models.ForeignKey(User, default=1)
+    publisher = models.CharField(max_length=100)
+    release_date = models.DateField(default=datetime(1970, 1, 1))
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length = 100)
+    sex = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    is_group = models.BooleanField()
+
+
+class Track(models.Model):
+    filename = models.CharField(max_length=100)
+    artist = models.ForeignKey(models.Artis)
+    album = models.ForeignKey(models.Album)
+    is_lyrics_available = models.BooleanField()
+    lyrics = models.TextField(max_length=500)
