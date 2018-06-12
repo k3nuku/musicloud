28cbc044026e8394c4b9afcf6ba0266fccd6c834
k3nuku <k3nuku@gmail.com>
2018-06-13 03:29:07 +0900

    [Add] FileField, ImageField support to models.py

--- mc/models.py
+++ mc/models.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 class Artist(models.Model):
     name = models.CharField(max_length=100)
-    images = models.CharField(max_length=200, blank=True)
+    images = models.ImageField(upload_to='images/artist', blank=True)
     is_group = models.BooleanField()
 
     def __str__(self):
@@ -13,7 +13,7 @@ class Artist(models.Model):
 
 class Album(models.Model):
     name = models.CharField(max_length=255)
-    images = models.CharField(max_length=200, blank=True)
+    images = models.ImageField(upload_to='images/album', blank=True)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     publisher = models.CharField(max_length=100)
     release_date = models.DateField(default=datetime(1970, 1, 1))
@@ -26,7 +26,7 @@ class Track(models.Model):
     name = models.CharField(max_length=250)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
-    filename = models.CharField(max_length=100)
+    media = models.FileField(upload_to='tracks')
     is_lyrics_available = models.BooleanField()
     lyrics = models.TextField(max_length=500, null=True, blank=True)
 
