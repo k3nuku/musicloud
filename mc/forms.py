f9e71aff61281c4997b871147e4c29ae88b25174
k3nuku <k3nuku@gmail.com>
2018-06-12 19:29:18 +0900

    [Add] Login implementation

---
+++ mc/forms.py
@@ -0,0 +1,40 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from mc.models import Album, Track, Artist
+
+
+class ArtistForm(forms.ModelForm):
+    class Meta:
+        model = Artist
+        fields = ['name', 'is_group']
+
+
+class AlbumForm(forms.ModelForm):
+    class Meta:
+        model = Album
+        fields = ['name', 'artist', 'publisher', 'release_date']
+
+
+class TrackForm(forms.ModelForm):
+    class Meta:
+        model = Track
+        fields = ['name', 'artist', 'album', 'filename', 'is_lyrics_available', 'lyrics']
+
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    nickname = forms.CharField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super(RegistrationForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+
+        if commit:
+            user.save()
+
+        return user
--- mc/views.py
+++ mc/views.py
@@ -1,60 +1,23 @@
-from django.contrib.auth import authenticate, login
-from django.contrib.auth import logout
-
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login, logout
-from musicloud.forms import AlbumForm, TrackForm, UserForm
-from mc.models import Album, Track, Artist
-
-
-# Create your views here.
-
-
-def login_user(request):
-    if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                # login success
-                return render(request, 'template/index.html', {})
-            return  # account is not active
-        else:
-            return  # login fail
-
-
-def logout_user(request):
-    logout(request)
-    form = UserForm(request.POST or None)
-    context = {
-        "form": form,
-    }
-    return render(request, 'template/index.html')
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from mc.forms import RegistrationForm
 
 
 def index(request):
-    return render(request, 'template/index.html')
+    return render(request, 'index.html')
 
 
 def register(request):
-    form = UserForm(request.POST or None)
-    if form.is_valid():
-        user = form.save(commit=False)
-        username = form.cleaned_data['username']
-        password = form.cleaned_data['password']
-        user.set_password(password)
-        user.save()
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                return render(request, 'template/index.html', )
-            else:
-                return render(request, '')
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
 
-    pass
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        else:
+            return HttpResponse('form is not valid')
+
+    return render(request, 'register.html', {'form': RegistrationForm})
 
 
 def search(request):
